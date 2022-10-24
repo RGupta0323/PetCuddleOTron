4a78ef4e8e7c8d2a64101d73a67cc3ff74d302cd
Rohan Gupta <36019794+RGupta323@users.noreply.github.com>
2022-10-23 18:34:40 -0700

    Update DevDeploy

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ from pet_cuddle_o_tron.pet_cuddle_o_tron_stack import PetCuddleOTronStack
 
 
 app = cdk.App()
-PetCuddleOTronStack(app, "pet-cuddle-o-tron")
+PetCuddleOTronStack(app, "pet-cuddle-o-tron-cdk-stack")
 
 app.synth()
