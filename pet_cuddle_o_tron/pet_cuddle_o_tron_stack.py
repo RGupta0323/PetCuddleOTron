92631ddb725234f59c46301f8956e74f69d43e03
Rohan Gupta <36019794+RGupta323@users.noreply.github.com>
2022-10-23 17:36:50 -0700

    sns sms logic

--- pet_cuddle_o_tron/pet_cuddle_o_tron_stack.py
+++ pet_cuddle_o_tron/pet_cuddle_o_tron_stack.py
@@ -1,4 +1,5 @@
 from constructs import Construct
+import boto3
 from aws_cdk import (
     Duration,
     Stack,
@@ -18,6 +19,10 @@ class PetCuddleOTronStack(Stack):
 
         # STage 1: Setting up SES service
         email_sns_topic = sns.Topic(self, "PetCuddleOTronStack_SNS_Email")
+
+        sns_client = boto3.client("sns")
+        sns_client.subscribe(topic="PetCuddleOTronStack_SNS_Email", endpoint="sms", protocol="5109962934")
+        sns_client.close()
 
 
         ses.ReceiptRuleSet(self, "PetCuddleOTrongStack_SES_RuleSet",
@@ -39,5 +44,6 @@ class PetCuddleOTronStack(Stack):
                            )
                            ]
                            )
+
 
 
