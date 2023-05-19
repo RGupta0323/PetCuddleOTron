c5ef6722a34a9bafddf7e8019a7fc1caeef10142
RGupta0323 <guptarohan323@gmail.com>
2023-05-19 13:19:03 -0500

    added some more stuff in api_lambda pytest

--- software/tests/unit/test_api_lambda.py
+++ software/tests/unit/test_api_lambda.py
@@ -2,5 +2,6 @@ import pytest
 from software.src.api_lambda import get_sm_arn
 
 def test_get_sm_arn(): 
-    print(get_sm_arn())
-    assert get_sm_arn() != None 
+    result = get_sm_arn() 
+    assert type(result) == str 
+    assert "arn" in result and "aws" in result and "states" in result and "us-east-1" in result 
